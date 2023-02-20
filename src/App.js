98f78430042c3e1654aab6edab8fb3b21f751076
Olatunji Ayobami Samson <114535570+haywhyvilla@users.noreply.github.com>
2023-02-20 10:54:36 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { useEffect } from "react";
+
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+// 1b4892e0
+
+const API_URL = "http://www.omdbapi.com?apikey=1b4892e0";
+
+const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data);
+  };
+  useEffect(() => {}, []);
+
+  return;
+
+  <h1>App</h1>;
+};
 
 export default App;
