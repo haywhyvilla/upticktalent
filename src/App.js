64edae6430dd047a9744f5b32e3fb809c835ffe3
Olatunji Ayobami Samson <114535570+haywhyvilla@users.noreply.github.com>
2023-02-22 01:12:43 +0100

    alot

--- src/App.js
+++ src/App.js
@@ -36,10 +36,14 @@ const App = () => {
       <div className="search">
         <input
           placeholder="Search for movies"
-          value="Superman"
-          onChange={() => {}}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <img src={SearchIcon} alt="Search" onClick={() => {}} />
+        <img
+          src={SearchIcon}
+          alt="Search"
+          onClick={() => searchMovies(searchTerm)}
+        />
       </div>
 
       {movies?.length > 0 ? (
